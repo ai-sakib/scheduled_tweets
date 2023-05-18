e65d36f0067e337067338ca3d1642ae9ae8c0339
Md Asiful Islam Sakib <sakib2439@gmail.com>
2023-05-18 19:43:36 +0600

    chore: Add registration form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get "about-us", to: "about#index", as: :about
+  
+  get "register", to: "registrations#new"
+  post "register", to: "registrations#create"
 end
